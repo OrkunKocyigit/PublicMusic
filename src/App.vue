635b4cabc420231b4e1fb14903d16abd439678bc
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2020-08-13 13:34:44 +0300

    Added basic data structures

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,10 @@
   <div id="app">
     <div v-if="!start">
       Start discovering new music
-      <button v-on:click="start = !start">Start</button>
+      <button v-on:click="createList">Start</button>
     </div>
     <div v-else>
-      <iframe
-      width="0"
-      height="0"
-      src="https://www.youtube.com/embed/FShbpXy-xAo?autoplay=1&controls=0&start=70&end=90&fs=0&modestbranding=1&cc_load_policy=1"
-      frameborder="0" 
-      allow="autoplay" >
-      </iframe>
+      <iframe width="0" height="0" :src="currentUrl" frameborder="0" allow="autoplay"></iframe>
       <div v-if="hidden">
         <button v-on:click="hidden = !hidden">Reveal</button>
         <button v-on:click="nextSong">Skip</button>
@@ -23,28 +17,48 @@
       </div>
     </div>
     <div>
-        <button v-on:click="nextSong">Rock</button>
-        <button v-on:click="nextSong">Pop</button>
-        <button v-on:click="nextSong">Classic</button>
+      <button v-for="genre in genres" :key="genre.id">{{genre.name}}</button>
     </div>
   </div>
 </template>
 
 <script>
+import songsJSON from "./assets/songs";
+import genresJSON from "./assets/genres";
 export default {
-  name: 'App',
+  name: "App",
   data: () => {
     return {
       start: false,
-      hidden: true
-    }
+      hidden: true,
+      songs: songsJSON,
+      genres: genresJSON,
+      index: 0,
+    };
   },
   methods: {
-    nextSong: () => {
-
-    }
-  }
-}
+    nextSong() {
+      this.index += 1;
+    },
+    createList() {
+      this.start = true;
+    },
+  },
+  computed: {
+    currentUrl() {
+      let song = this.songs[this.index];
+      let url = "";
+      switch (song.provider) {
+        case "youtube":
+          url =
+            "https://www.youtube.com/embed/" +
+            song.id +
+            "?autoplay=1&controls=0&start=0&end=30&fs=0&modestbranding=1&cc_load_policy=1";
+      }
+      return url;
+    },
+  },
+};
 </script>
 
 <style>
