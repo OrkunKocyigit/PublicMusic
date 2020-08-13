807f19e26d15b21d0a646ff33110d160e1b493e7
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2020-08-13 12:48:31 +0300

    Created Basic Template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="!start">
+      Start discovering new music
+      <button v-on:click="start = !start">Start</button>
+    </div>
+    <div v-else>
+      <iframe
+      width="0"
+      height="0"
+      src="https://www.youtube.com/embed/FShbpXy-xAo?autoplay=1&controls=0&start=70&end=90&fs=0&modestbranding=1&cc_load_policy=1"
+      frameborder="0" 
+      allow="autoplay" >
+      </iframe>
+      <div v-if="hidden">
+        <button v-on:click="hidden = !hidden">Reveal</button>
+        <button v-on:click="nextSong">Skip</button>
+      </div>
+      <div v-else>
+        <button v-on:click="nextSong">Favorite</button>
+        <button v-on:click="nextSong">Go to Source</button>
+        <button v-on:click="nextSong">Next Song</button>
+      </div>
+    </div>
+    <div>
+        <button v-on:click="nextSong">Rock</button>
+        <button v-on:click="nextSong">Pop</button>
+        <button v-on:click="nextSong">Classic</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      start: false,
+      hidden: true
+    }
+  },
+  methods: {
+    nextSong: () => {
+
+    }
   }
 }
 </script>
